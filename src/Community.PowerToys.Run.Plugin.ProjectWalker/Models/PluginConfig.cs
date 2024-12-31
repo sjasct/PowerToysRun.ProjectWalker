69ea6dd96a4dea0c7f58439a570bca15f378fe1f
Sam <sam@samjas.co.uk>
2024-12-31 12:24:32 +0000

    change default project structure

--- src/Community.PowerToys.Run.Plugin.ProjectWalker/Models/PluginConfig.cs
+++ src/Community.PowerToys.Run.Plugin.ProjectWalker/Models/PluginConfig.cs
@@ -6,7 +6,7 @@ public class PluginConfig
 {
     public string BasePath { get; set; } = string.Empty;
     public string? CustomEditorExecutablePath { get; set; }
-    public FolderStructureType FolderStructureType { get; set; } = FolderStructureType.ProjectParents;
+    public FolderStructureType FolderStructureType { get; set; } = FolderStructureType.StandaloneRepos;
     public int SearchMatchRatio { get; set; } = 70;
     public List<string> IgnoredFolders { get; set; } = []; 
     public List<OpenOption> Options { get; set; } = [];
